25f22dd6cd3a7af8fa6a415b49c411d74a9be126
swaroop0202 <swaroopk372@gmail.com>
2024-07-26 18:23:44 +0530

    M

--- instances.sh
+++ instances.sh
@@ -6,10 +6,9 @@ for name in ${instances[@]};do
  echo "creating instances:$name"
 
  if[ $name == "shipping" ]  || [ $name== "mysql" ] 
-
-    then
-        echo "instance shoul be t3.small"
-    else
+ then
+    echo "instance shoul be t3.small"
+ else
         echo "instance should be t3.micro"
  fi   
 done
