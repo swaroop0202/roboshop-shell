aa225dfe8753543faa249e022fbb633eb35e993b
swaroop0202 <swaroopk372@gmail.com>
2024-07-26 18:25:45 +0530

    M

--- instances.sh
+++ instances.sh
@@ -5,10 +5,11 @@ instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue"  "users" "cart" "shi
 for name in ${instances[@]};do
  echo "creating instances:$name"
 
- if[ $name == "shipping" ]  || [ $name== "mysql" ] 
- then
-    echo "instance shoul be t3.small"
- else
+   if[ $name == "shipping" ]  || [ $name== "mysql" ] 
+   then
+
+        echo "instance shoul be t3.small"
+   else
         echo "instance should be t3.micro"
- fi   
+   fi   
 done
