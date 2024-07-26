6eef15e3f7256c79ae465dbe1f428f4e66f08377
swaroop0202 <swaroopk372@gmail.com>
2024-07-26 18:35:45 +0530

    M

--- instances.sh
+++ instances.sh
@@ -12,3 +12,5 @@ for name in ${instances[@]};do
         echo "instance should be t3.micro"
    fi   
 done
+
+aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --count 1 --instance-type t2.micro  --security-group-ids sg-0cb50d0db2f7c8084 --subnet-id subnet-0d28f9a915b78dd57
