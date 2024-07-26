e7a293b053eafa0833d903b5b4ab6b9149108c31
swaroop0202 <swaroopk372@gmail.com>
2024-07-26 18:09:31 +0530

    M

--- instances.sh
+++ instances.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-for name in {"mongodb","redis","mysql","rabbitmq","catalogue","users","cart","shipping","payments","web"}
+instances=("mongodb","redis","mysql","rabbitmq","catalogue","users","cart","shipping","payments","web")
+
+for name in {$instances[@]}
 do 
  echo "creating instances:$name"
+ if []
 done
