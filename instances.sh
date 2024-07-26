144868b1f3a8409fef0678bfa63ffe0461af19b9
swaroop0202 <swaroopk372@gmail.com>
2024-07-26 19:11:15 +0530

    M

--- instances.sh
+++ instances.sh
@@ -7,11 +7,11 @@ for name in ${instances[@]};do
 
    if [ $name == "shipping" ]  || [ $name == "mysql" ] 
    then
-        instance_type = "t3.medium"
+        instance_type="t3.medium"
    else
-        instance_type = "t3.micro"
+        instance_type="t3.micro" 
    fi  
-  echo "creating instance for $name : instance type is $instance_type" 
+  echo "creating instance for $name : instance type is $instance_type " 
    instance_id=$(aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --instance-type $instance_type  --security-group-ids sg-0cb50d0db2f7c8084 --subnet-id subnet-0d28f9a915b78dd57 --query 'Instances[*].InstanceId' --output text)
 
    echo "printing public and private ips"
