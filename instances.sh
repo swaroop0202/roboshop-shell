bdcb86897d98709cf99b26369fc1a35463354ca5
swaroop0202 <swaroopk372@gmail.com>
2024-07-26 18:17:11 +0530

    M

--- instances.sh
+++ instances.sh
@@ -2,6 +2,6 @@
 
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue"  "users" "cart" "shipping" "payments" "web")
 
-for name in {$instances};do
+for name in ${instances[@]};do
  echo "creating instances:$name"
 done
