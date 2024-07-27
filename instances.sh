b47cb4e450ceb17547ce762d2d4f0f518d66f0ae
swaroop0202 <swaroopk372@gmail.com>
2024-07-27 14:11:44 +0530

    M

--- instances.sh
+++ instances.sh
@@ -11,7 +11,7 @@ for name in ${instances[@]};do
    else
         instance_type="t3.micro"    
    fi  
-  echo "creating instance for $name : instance type is $instance_type " 
+   echo "creating instance for $name : instance type is $instance_type " 
    instance_id=$(aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --instance-type $instance_type  --security-group-ids sg-0cb50d0db2f7c8084 --subnet-id subnet-0d28f9a915b78dd57 --query 'Instances[0].InstanceId' --output text)
 
    aws ec2 create-tags --resources $instance_id --tags Key=project,Value=$name 
